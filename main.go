97ccd08f075c2c8df2336afb5b77d57ec9e1c467
Dajan Medina <dmedina2150dev@gmail.com>
2026-01-08 12:41:42 -0300

    Update repo

--- main.go
+++ main.go
@@ -3,18 +3,37 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
 	fmt.Println("Server Running")
-	fs := http.FileServer(http.Dir("static/"))
+
+	// TODO: Server sin libs de terceros
+	// fs := http.FileServer(http.Dir("static/"))
 	// http.Handle("/static/", http.StripPrefix("/static/", fs))
+	// http.Handle("/", http.StripPrefix("/", fs))
+	// http.HandleFunc("/api/hello", func(w http.ResponseWriter, r *http.Request) {
+	// 	fmt.Fprintf(w, "Welcome Hello")
+	// })
+	// http.ListenAndServe(":80", nil) // el nil hace referencia al parametro del enrutador principal del server
 
-	http.Handle("/", http.StripPrefix("/", fs))
-
-	http.HandleFunc("/api/hello", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Welcome Hello")
+	// TODO: Server con un MANEJADOR de REQUEST de terceros
+	r := mux.NewRouter()
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello word")
 	})
 
-	http.ListenAndServe(":80", nil)
+	r.HandleFunc("/books/{title}/page/{page}", func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		fmt.Println(vars)
+		title := vars["title"]
+		page := vars["page"]
+
+		fmt.Printf("Params: title: %s, page: %s\n", title, page)
+		fmt.Fprintf(w, "You've requested the book: %s on page %s\n", title, page)
+	})
+
+	http.ListenAndServe(":80", r)
 }
